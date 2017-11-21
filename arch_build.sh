5318f943705f5cc91822772e18c35485c5a13c38
dmsun <johnson.michael.david@gmail.com>
2017-11-21 13:56:47 +1100

    test arch_build.sh

--- arch_build.sh
+++ arch_build.sh
@@ -11,11 +11,10 @@ BASH=bash-completion
 UTIL=openssh rsync samba transmission-gtk unrar wireless_tools gparted gzip htop imagemagick bzip2 deepin-screenshot ecm-tools
 
 #FONTS
-FONTS=
 AUR_FONTS=ttf-ms-fonts numix-circle-icon-theme-git numix-gtk-theme-git numix-icon-theme-git paper-gtk-theme-git paper-icon-theme 
 
 #SOUND
-SOUND=alsa-firmware alsa-tools alsa-utils pulseaudio pulseaudio-bluetooth pulseaudio-equalizer pulseaudio-gconf pulseaudio-jack 
+SOUND=alsa-firmware alsa-tools alsa-utils pulseaudio pulseaudio-bluetooth pulseaudio-equalizer pulseaudio-gconf pulseaudio-jack
 
 #GRAPHICS
 GRAPHICS=nvidia nvidia-settings opencl-nvidia ocl-icd vulkan-icd-loader
@@ -24,11 +23,11 @@ GRAPHICS=nvidia nvidia-settings opencl-nvidia ocl-icd vulkan-icd-loader
 XSERV=xorg-server xorg-twm xorg-xclock xorg-xinit
 
 #GNOME
-GNOME= gdm gnome-backgrounds gnome-themes-standard networkmanager nautilus mousetweaks awaita-icon-theme
-GNOME_EXTRA=(evolution gnome-calendar gnome-photos gnome-tweak-tool dconf-editor brasero)
+GNOME=gdm gnome-backgrounds gnome-themes-standard networkmanager nautilus mousetweaks awaita-icon-theme
+GNOME_EXTRA=evolution gnome-calendar gnome-photos gnome-tweak-tool dconf-editor brasero
 
 #OFFICE
-OFFICE = libreoffice-fresh 
+OFFICE =libreoffice-fresh 
 
 #WEBBROWSER
 CHROME= chrome-gnome-shell-git chromium
@@ -45,8 +44,8 @@ DATA=opencv octave
 AUR_DATA=anaconda #maybedothismanually
 
 #AUR
-AUR= conky-colors-git conky-nvidia archey3 discord dropbox nautilus-dropbox nautilus-admin rednotebook spotify tilix whatsapp-desktop discord
+AUR=conky-colors-git conky-nvidia archey3 discord dropbox nautilus-dropbox nautilus-admin rednotebook spotify tilix whatsapp-desktop discord
 
-
+sudo pacman -Syu ${BASH} ${SOUND}
 
 
