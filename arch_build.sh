5c49d366e751d67d8988e62b7072e8dfe2640023
dmsun <johnson.michael.david@gmail.com>
2017-11-21 14:25:45 +1100

    changed brackets for inverted commas

--- arch_build.sh
+++ arch_build.sh
@@ -1,4 +1,4 @@
-#! usr/bin/env bash
+#! /usr/bin/env bash
 # to run:
 # wget raw.githubusercontent.com/dmsun/Arch-Linux-Config/master/arch_build.sh
 #chmod +x arch_build.sh
@@ -8,43 +8,43 @@
 BASH=bash-completion
 
 #UTILITIES
-UTIL=(openssh rsync samba transmission-gtk unrar wireless_tools gparted gzip htop imagemagick bzip2 deepin-screenshot ecm-tools)
+UTIL="openssh rsync samba transmission-gtk unrar wireless_tools gparted gzip htop imagemagick bzip2 deepin-screenshot ecm-tools"
 
 #FONTS
-AUR_FONTS=(ttf-ms-fonts numix-circle-icon-theme-git numix-gtk-theme-git numix-icon-theme-git paper-gtk-theme-git paper-icon-theme)
+AUR_FONTS="ttf-ms-fonts numix-circle-icon-theme-git numix-gtk-theme-git numix-icon-theme-git paper-gtk-theme-git paper-icon-theme"
 
 #SOUND
-SOUND=(alsa-firmware alsa-tools alsa-utils pulseaudio pulseaudio-bluetooth pulseaudio-equalizer pulseaudio-gconf pulseaudio-jack)
+SOUND="alsa-firmware alsa-tools alsa-utils pulseaudio pulseaudio-bluetooth pulseaudio-equalizer pulseaudio-gconf pulseaudio-jack"
 
 #GRAPHICS
-GRAPHICS=(nvidia nvidia-settings opencl-nvidia ocl-icd vulkan-icd-loader)
+GRAPHICS="nvidia nvidia-settings opencl-nvidia ocl-icd vulkan-icd-loader"
 
 #XSERVER
-XSERV=(xorg-server xorg-twm xorg-xclock xorg-xinit)
+XSERV="xorg-server xorg-twm xorg-xclock xorg-xinit"
 
 #GNOME
-GNOME=(gdm gnome-backgrounds gnome-themes-standard networkmanager nautilus mousetweaks awaita-icon-theme)
-GNOME_EXTRA=(evolution gnome-calendar gnome-photos gnome-tweak-tool dconf-editor brasero)
+GNOME="gdm gnome-backgrounds gnome-themes-standard networkmanager nautilus mousetweaks awaita-icon-theme"
+GNOME_EXTRA="evolution gnome-calendar gnome-photos gnome-tweak-tool dconf-editor brasero"
 
 #OFFICE
-OFFICE =libreoffice-fresh 
+OFFICE=libreoffice-fresh 
 
 #WEBBROWSER
-CHROME=(chrome-gnome-shell-git chromium)
+CHROME="chrome-gnome-shell-git chromium"
 
 #MEDIA
-MEDIA=(vlc deepin-screenshot kodi)
+MEDIA="vlc deepin-screenshot kodi"
 
 #GAMES
-GAMES=(openra dolphin-emu ppsspp steam)
-AUR_GAMES=(gzdoom itch pcsx2-git)
+GAMES="openra dolphin-emu ppsspp steam"
+AUR_GAMES="gzdoom itch pcsx2-git"
 
 #DATASCIENCE
-DATA=(opencv octave)
+DATA="opencv octave"
 AUR_DATA=anaconda #maybedothismanually
 
 #AUR
-AUR=(conky-colors-git conky-nvidia archey3 discord dropbox nautilus-dropbox nautilus-admin rednotebook spotify tilix whatsapp-desktop discord)
+AUR="conky-colors-git conky-nvidia archey3 discord dropbox nautilus-dropbox nautilus-admin rednotebook spotify tilix whatsapp-desktop discord"
 
 sudo pacman -Syu ${BASH} ${SOUND}
 
--- arch_install.sh
+++ arch_install.sh
@@ -1,4 +1,4 @@
-#! usr/bin/env bash
+#! /usr/bin/env bash
 # to run:
 # wget raw.githubusercontent.com/dmsun/Arch-Linux-Config/master/arch_install.sh
 #chmod +x arch_install.sh
@@ -66,7 +66,7 @@ vim /etc/pacman.d/mirrorlist \
 '+r ! grep "'"${MIRROR}"'" -A 1 % | grep -v "^--"' \
 '+wq'
 
-local CORE_PACKAGES=(base base-devel grub openssh sudo ntp wget neovim iw wpa_supplicant dialog wireless_toolsq)
+local CORE_PACKAGES="base base-devel grub openssh sudo ntp wget neovim iw wpa_supplicant dialog wireless_toolsq"
 pacman-key --refresh-keys
 pacstrap /mnt/ ${CORE_PACKAGES}
 genfstab -U /mnt/ >> /mnt/etc/fstab
