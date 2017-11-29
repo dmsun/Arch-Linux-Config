a9fd760a57746a79cbb87f14a3332b486793815c
dmsun <johnson.michael.david@gmail.com>
2017-11-29 16:38:50 +1100

    removing backup_config.sh

--- backup_config.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env bash
-# to run:
-# wget raw.githubusercontent.com/dmsun/Arch-Linux-Config/master/backup_config_files.sh
-#chmod +x backup_config_files.sh
-#./backup_config_files.sh
-
