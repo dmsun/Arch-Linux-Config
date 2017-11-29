8448f2214860e671005935c7e798f1d2eeea4ede
dmsun <johnson.michael.david@gmail.com>
2017-11-29 13:43:47 +1100

    adding conditions on gpu

--- arch_build.sh
+++ arch_build.sh
@@ -8,7 +8,10 @@
 BASH=bash-completion
 
 #GRAPHICS
-GRAPHICS="nvidia nvidia-settings opencl-nvidia ocl-icd vulkan-icd-loader"
+#this will depend on your build, if you're using an AMD gpu, use AMDGPU.
+# try lspci | grep -e VGA -e 3D
+GRAPHICS_NVIDIA="nvidia nvidia-utils lib32-nvidia-utils nvidia-settings opencl-nvidia ocl-icd vulkan-icd-loader"
+GRAPHICS_AMD="mesa lib32-mesa amdgpu xf86-video-amdgpu opencl-nvidia ocl-icd vulkan-radeon"
 
 #SOUND
 SOUND="alsa-firmware alsa-tools alsa-utils pulseaudio pulseaudio-bluetooth pulseaudio-equalizer pulseaudio-gconf pulseaudio-jack"
@@ -46,9 +49,21 @@ AUR_DATA=anaconda #maybedothismanually
 #AUR
 AUR="conky-colors-git conky-nvidia archey3 discord dropbox nautilus-dropbox nautilus-admin rednotebook spotify tilix whatsapp-desktop discord"
 
+#Install groups line by line in order to minimise clashes with dependencies.
 sudo pacman -Syu ${BASH}
 sudo pacman -Syu ${UTIL}
 sudo pacman -Syu ${SOUND}
-sudo pacman -Syu
+
+read -p "Is your GPU AMD or NVIDIA (AMD/NVIDIA)? " answer
+case ${answer:0:1} in
+    a|A )
+        echo "You have selected AMD"
+    ;;
+    * )
+        echo "You have selected NVIDIA"
+    ;;
+esac
+
+
 
 
