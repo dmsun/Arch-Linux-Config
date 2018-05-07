a4968603ddeb20db00ec67d9673d5612bf12341f
Michael Johnson <johnson.michael.david@gmail.com>
2018-05-07 18:24:30 +1000

    updating

---
+++ retropie/get_files.py
@@ -0,0 +1,15 @@
+#! /usr/bin python
+
+import os
+
+# change the directory to the rom hub
+cwd = os.chdir("/home/pi/RetroPie/roms")
+extensions = ['.srm', '.eep', '.state', '.hi', '.hs', '.fs'
+                                    '.dat', '.cfg', '.nv']
+def get_files(directory=os.getcwd()):
+    with open("/home/pi/backup_files.txt", 'w') as test:
+        for root, dir, files in os.walk(directory):
+            for name in files:
+                if name.endswith(tuple(extensions)):
+                    test.write(os.path.join(root, name))
+                    test.write("\n")
